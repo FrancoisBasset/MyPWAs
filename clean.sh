19f02cd4e6e8de926307eeae38d0581340d7c482
FrancoisBasset <francois@local>
2021-04-25 19:53:26 +0200

    maj

--- clean.sh
+++
@@ -1,5 +0,0 @@
-rm -r node_modules
-rm package-lock.json
-rm public/home/pwas.json
-rm -r pwas
-rm -r public/pwa1 public/pwa2 public/pwa3 public/rocketbookupdate public/trancedownloader public/jetuilnousvousils
